220bab5ea94738e6b8b6af4cb4bb23d2e18c6cda
Apurva Gawande <agawande@bizstream.com>
2025-02-14 17:37:12 -0500

    updated backend urls

--- app/api/hotelresult/hotellist/route.tsx
+++ app/api/hotelresult/hotellist/route.tsx
@@ -1,6 +1,7 @@
 import Amadeus from 'amadeus-ts';
 import { NextResponse } from 'next/server';
 
+const BASE_URL = process.env.NEXTAUTH_URL;
 // Initialize Amadeus client
 const amadeusClient = new Amadeus({
     clientId: process.env.NEXT_PUBLIC_AMADEUS_API_KEY,
@@ -17,7 +18,7 @@ export async function GET(request: Request) {
 
     try {        
         // 2. Getting Destination Location Code
-        const destinationData = await fetch(`http://localhost:3000/api/citycodes?keyword=${destinationKeyword}`);
+        const destinationData = await fetch(`${BASE_URL}/api/citycodes?keyword=${destinationKeyword}`);
         const destinationResponse = await destinationData.json();
         if (!destinationResponse.data || destinationResponse.data.length === 0) {
             return NextResponse.json({ error: 'No matching destination found' }, { status: 404 });
