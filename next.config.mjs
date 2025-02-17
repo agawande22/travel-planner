a16159a1095c65e1d2b712131c53ae138ca78a0a
agawande22 <agawande@bizstream.com>
2025-02-16 19:42:16 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
             protocol: 'https',
             hostname: 'google.com',
             port: '',
-            pathname: '/maps/place/**'
+            pathname: '/maps/place/?q=place_id:**'
           },
         ],
       },
